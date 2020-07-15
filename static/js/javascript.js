50bdbc13b7e62ed7e764b620c4a5589d9aba116c
Daniel Clements <daniel.clements14@gmail.com>
2020-07-15 15:20:07 +0000

    adde comments

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,4 +1,5 @@
- $(document).ready(function () { 
+ $(document).ready(function () {
+        // multiple select by bootstrap select 
        $('select').selectpicker();
 
     var slider = document.getElementById("review_score");
